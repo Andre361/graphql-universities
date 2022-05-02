58e0b4f837e3093a2ade908d17a90a65b3ca91e4
J <Joeydre69@gmail.com>
2022-05-02 04:37:11 +0100

    create university model and its migrations

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class University(models.Model):
+    name = models.TextField()
+    country = models.CharField(max_length=56)
+    alpha_two_code = models.CharField(max_length=2)
+    web_pages = ArrayField(models.TextField())
+    domains = ArrayField(models.TextField())
+    state_province = models.TextField()
