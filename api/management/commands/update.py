78e0587f9a8e426439f36e6df5ba1a09769ca679
J <Joeydre69@gmail.com>
2022-05-19 01:54:09 +0100

    refactored update command

--- api/management/commands/update.py
+++ api/management/commands/update.py
@@ -22,25 +22,11 @@ def update_database():
             state_province=x["state-province"],
             domains=x["domains"],
         ))
-    saved_objects = list(University.objects.all())
-    objects_to_create = [i for i in objects if i not in saved_objects]
-    return University.objects.bulk_create(objects_to_create,ignore_conflicts=True)
-
+    
+    University.objects.bulk_create(objects,ignore_conflicts=True)
 class Command(BaseCommand):
     help="updates the database when new objects are added"
     
     def handle(self,*args,**options):
-        data = fetch()
-        objects = []
-        for x in data:
-            objects.append(University(
-                country=x["country"],
-                name=x["name"],
-                web_pages=x["web_pages"],
-                alpha_two_code=x["alpha_two_code"],
-                state_province=x["state-province"],
-                domains=x["domains"],
-            ))
-        
-        University.objects.bulk_create(objects,ignore_conflicts=True)
-        self.stdout.write(self.style.SUCCESS('Successfully closed updated'))
+        update_database()
+        self.stdout.write(self.style.SUCCESS('Successfully updated database'))
