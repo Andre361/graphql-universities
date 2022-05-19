c5fcf5ca1250e08cca38d9379c76fafb07c4c969
J <Joeydre69@gmail.com>
2022-05-19 03:12:27 +0100

    removed django-backgroundtasks4

--- config/settings.py
+++ config/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party apps
     'graphene_django',
-    'background_task',
-    
+    # local apps
     'api',
 
 ]
